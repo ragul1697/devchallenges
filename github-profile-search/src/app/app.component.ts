389862dddc64c45e0122f304ce703f375b83263d
ragul.u <ragul.u@kgisl.com>
2024-04-26 17:37:30 +0530

    Added profile and repositories section

--- github-profile-search/src/app/app.component.ts
+++ github-profile-search/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GithubProfileService } from './github-profile.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [GithubProfileService]
@@ -15,12 +16,30 @@ export class AppComponent implements OnInit {
 
   constructor(private githubProfileService: GithubProfileService) { }
 
-  title: string = 'github-profile-search';
+  public title: string = 'github-profile-search';
+
+  public usernameControl: FormControl = new FormControl<string | null>(null);
+
+  public githubRepoDetails: any[] = [];
 
   ngOnInit(): void {
-    this.githubProfileService.getGithubProfileResults('github').subscribe({
+    if (false) {
+      this.fetchGithubDetails('github');
+    }
+  }
+
+  public onUserSearchClick() {
+    const username = this.usernameControl.value;
+    if (username !== null) {
+      this.fetchGithubDetails(username);
+    }
+  }
+
+  private fetchGithubDetails(inputName: string) {
+    this.githubProfileService.getGithubProfileResults(inputName).subscribe({
       next: (response: any) => {
         console.log(response);
+        this.githubRepoDetails = response;
       }
     });
   }
